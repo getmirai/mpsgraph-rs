cedf26e0a2aa2dcb8b2d9583d5b160f89dbe025c
Eugene Bokhan <eugenebokhan@icloud.com>
2025-07-19 23:11:00 +0100

    update deployment platform

--- src/executable/deployment_platform.rs
+++ src/executable/deployment_platform.rs
@@ -1,6 +1,10 @@
+use objc2::{Encode, Encoding, RefEncode};
+
 /// Represents the deployment platform for a graph
+///
+/// See also [Apple's documentation](https://developer.apple.com/documentation/metalperformanceshadersgraph/mpsgraphdeploymentplatform?language=objc)
 #[repr(u64)]
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub enum DeploymentPlatform {
     /// macOS platform
     MacOS = 0,
@@ -10,4 +14,12 @@ pub enum DeploymentPlatform {
     TVOS = 2,
     /// visionOS platform
     VisionOS = 3,
+}
+
+unsafe impl Encode for DeploymentPlatform {
+    const ENCODING: Encoding = u64::ENCODING;
+}
+
+unsafe impl RefEncode for DeploymentPlatform {
+    const ENCODING_REF: Encoding = Encoding::Pointer(&Self::ENCODING);
 }
