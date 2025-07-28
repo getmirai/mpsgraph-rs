8a4aa41421e4b5e56de49011b995ec4bc2abe114
Eugene Bokhan <eugenebokhan@icloud.com>
2025-07-28 19:31:45 +0100

    update aliasing strategy

--- src/core/mps_types/aliasing_strategy.rs
+++ src/core/mps_types/aliasing_strategy.rs
@@ -2,23 +2,20 @@ use objc2::Encoding;
 use objc2::{Encode, RefEncode};
 use objc2_foundation::NSUInteger;
 
-bitflags::bitflags! {
-    #[allow(dead_code)]
-    #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-    pub struct AliasingStrategy: NSUInteger {
-        const DEFAULT = 0;
-        const DONT_CARE = Self::DEFAULT.bits();
-        const SHALL_ALIAS = 1 << 0;
-        const SHALL_NOT_ALIAS = 1 << 1;
-        const ALIASING_RESERVED = Self::SHALL_ALIAS.bits() | Self::SHALL_NOT_ALIAS.bits();
-        const PREFER_TEMPORARY_MEMORY = 1 << 2;
-        const PREFER_NON_TEMPORARY_MEMORY = 1 << 3;
-    }
+#[repr(usize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
+pub enum AliasingStrategy {
+    DontCare = 0,
+    ShallAlias = 1 << 0,
+    ShallNotAlias = 1 << 1,
+    AliasingReserved = (Self::ShallAlias as usize | Self::ShallNotAlias as usize),
+    PreferTemporaryMemory = 1 << 2,
+    PreferNonTemporaryMemory = 1 << 3,
 }
 
-impl From<AliasingStrategy> for NSUInteger {
-    fn from(flags: AliasingStrategy) -> Self {
-        flags.bits()
+impl Default for AliasingStrategy {
+    fn default() -> Self {
+        Self::DontCare
     }
 }
 
